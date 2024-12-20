46637386953684f26c444edd11ccd48cad5b42ca
IEMDomain04.dc <emmanman0011@gmail.com>
2024-12-20 22:53:31 +0800

    Added files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <>
-      <h1 className="px-10 py-10 bg-green-600">HELLO WORLD</h1>
+      <div>
+        
+      </div>
     </>
   );
 }
---
+++ src/app/sections/Topnav.tsx
@@ -0,0 +1,35 @@
+export default function Topnav() {
+  return (
+    <>
+      <div>
+        {/* Top Nav */}
+        <div>
+
+          {/* Logo and Title of Compiler */}
+          <div>
+            <img src="" alt="" />
+            <h1></h1>
+          </div>
+
+          {/* Saves and runs */}
+          <div>
+            <div>
+              <img src="" alt="" />
+              <h1>SAVE AS</h1>
+            </div>
+
+            <div>
+              <img src="" alt="" />
+              <h1>OPEN</h1>
+            </div>
+
+            <div>
+              <img src="" alt="" />
+              <h1>RUN</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
