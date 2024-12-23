eaf3ad1b0b8f390033a6e16d0ee75c69eb324079
IEMDomain04.dc <emmanman0011@gmail.com>
2024-12-23 20:32:58 +0800

    Organized File + Folder Structure

---
+++ src/app/semantic/page.tsx
@@ -0,0 +1,7 @@
+export default function Semantic() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
+  )
+}
---
+++ src/app/syntax/page.tsx
@@ -0,0 +1,7 @@
+export default function Syntax() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
