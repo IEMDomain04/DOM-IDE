33331514ac84e6785244d073ca391e3651fe2037
IEMDomain04.dc <emmanman0011@gmail.com>
2024-12-28 21:00:28 +0800

    Lexer and Token Table Adjusted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -159,7 +159,7 @@ curse domain(){
       </div>
 
       {/* Output Table for Lexeme, Tokens */}
-      <div className="flex flex-col w-5/12 mx-1">
+      <div className="flex flex-col w-5/12">
         <table className={`min-w-full table-fixed ${isDarkMode ? 'bg-dark-foreground text-white' : 'bg-light-foreground text-white'}`}>
           <thead className={`${isDarkMode ? 'bg-dark-foreground text-white' : 'bg-light-foreground text-white'}`}>
             <tr>
