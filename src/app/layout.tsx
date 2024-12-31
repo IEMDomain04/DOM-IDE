662534dceb66e0647cd9c7c8f02ae9f544575836
IEMDomain04.dc <emmanman0011@gmail.com>
2024-12-31 20:14:52 +0800

    Added OGP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 // TITLE OF THE WEB APP
 export const metadata: Metadata = {
   title: "DOM Compiler",
-  description: "DOM language (DOMAIN EXPANSION)",
+  description: "The DOM language IDE allows programming with a Jujutsu Kaisen-inspired theme (DOMAIN EXPANSION).",
 };
 
 export default function RootLayout({
@@ -25,6 +25,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:title" content="DOM Compiler" />
+        <meta property="og:description" content="The DOM language IDE allows programming with a Jujutsu Kaisen-inspired theme (DOMAIN EXPANSION)." />
+        <meta property="og:image" content="https://p325k7wa.twic.pics/high/jujutsu-kaisen/jujutsu-kaisen-cursed-clash/00-page-setup/JJK-header-mobile2.jpg?twic=v1/resize=760/step=10/quality=80" />
+        <meta property="og:url" content="https://dom-ide.vercel.app/" />
+        
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
