c01995ff0d5d02a4cdfe67470fd53291a91ef36b
edrill <edrillb@gmail.com>
2025-01-09 00:25:11 +0800

    Minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -163,15 +163,15 @@ curse domain(){
         <table className={`min-w-full table-fixed ${isDarkMode ? 'bg-dark-foreground text-white' : 'bg-light-foreground text-white'}`}>
           <thead className={`${isDarkMode ? 'bg-dark-foreground text-white' : 'bg-light-foreground text-white'}`} style={{ position: 'sticky', top: 0, zIndex: 1 }}>
             <tr>
-              <th className="py-3 px-4 text-l">Lexeme</th>
-              <th className="py-3 px-4 text-l">Tokens</th>
+              <th className="py-3 px-4 text-xl border-b-2 border-[#391d1d]">Lexeme</th>
+              <th className="py-3 px-4 text-xl border-b-2 border-[#391d1d]">Tokens</th>
             </tr>
           </thead>
           <tbody>
             {outputData.map((item, index) => (
               <tr key={index}>
-                <td className="py-2 px-4 border-1">{item.lexeme}</td>
-                <td className="py-2 px-4 border-1">{item.token}</td>
+                <td className="py-2 px-4 border-b-2 border-[#2f1919]">{item.lexeme}</td>
+                <td className="py-2 px-4 border-b-2 border-[#2f1919]">{item.token}</td>
               </tr>
             ))}
           </tbody>
