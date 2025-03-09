29953bad9c84c4c97a5dd23f122561ac2488ff86
edrill <edrillb@gmail.com>
2025-03-09 20:45:08 +0800

    More cleave bug fixes

--- semantic.py
+++ semantic.py
@@ -2123,65 +2123,66 @@ class MyASTVisitor(ASTVisitor):
             elif isinstance(node, CleaveNode):
                 true_parent = parent
                 while true_parent and not isinstance(true_parent, (VarDecNode, VarAssignNode, ClanIndexAssignNode, ClanDecNode)):
-                    true_parent = true_parent.paren
+                    true_parent = true_parent.parent
                 arg1 = node.arg1
                 arg2 = node.index1
                 arg3 = node.index2
                 arg1_type = self.infer_type(arg1)
                 arg2_type = self.infer_type(arg2)
                 arg3_type = self.infer_type(arg3)
-
+        
                 if isinstance(arg1, IdNode) and not self.symbol_table.get(arg1.name):
-                    self.errors.append(SemanticError(node.pos_start, node.pos_end, f"Undeclared variable 8 '{arg1.name}'"))
-                    continue
+                    self.unresolved_cases.append((node, parent))
                 else:
                     if isinstance(true_parent, (VarDecNode, VarAssignNode, ClanIndexAssignNode)):
+                        arg1_symbol = self.symbol_table.get(arg1.name)
+                        print(f'Arg1 Symbol: {arg1_symbol}')
+                        if arg1_symbol is None:
+                            self.errors.append(SemanticError(node.pos_start, node.pos_end, f"Undeclared variable 8 '{arg1.name}'"))
+                            continue
+                        else: 
+                            if isinstance(arg1_symbol, ClanDecNode):
+                                self.errors.append(SemanticError(node.pos_start, node.pos_end, f"Cannot assign clan to a non-clan variable"))
                         if not arg1_type == 'string':
-                            self.errors.append(SemanticError(node.pos_start, node.pos_end, f"Expected 'string', got '{arg1_type}'"))
+                            self.errors.append(SemanticError(node.pos_start, node.pos_end, f"Expected string for cleave first argument, got '{arg1_type}'"))
+                            continue
+                        if isinstance(true_parent, VarDecNode):
+                            if true_parent.datatype != 'string':
+                                self.errors.append(SemanticError(node.pos_start, node.pos_end, f"Expected '{true_parent.datatype}', got '{arg1_type}'"))
+                        elif isinstance(true_parent, (VarAssignNode, ClanIndexAssignNode)):
+                            if not self.symbol_table.get(true_parent.name):
+                                self.errors.append(SemanticError(node.pos_start, node.pos_end, f"Undeclared variable 9 '{true_parent.name}'"))
+                                continue
+                            else:
+                                if true_parent.datatype != 'string':
+                                    self.errors.append(SemanticError(node.pos_start, node.pos_end, f"Expected '{true_parent.datatype}', got '{arg1_type}'"))
+
                     elif isinstance(true_parent, ClanDecNode):
                         arg1_symbol = self.symbol_table.get(arg1.name)
                         if arg1_symbol is None:
-                            self.errors.append(SemanticError(node.pos_start, node.pos_end, f"Undefined clan 1 '{arg1.name}'"))
+                            self.errors.append(SemanticError(node.pos_start, node.pos_end, f"Undeclared variable 10 '{arg1.name}'"))
                             continue
                         elif not isinstance(arg1_symbol, ClanDecNode):
                             self.errors.append(SemanticError(node.pos_start, node.pos_end, f"Expected clan in first argument"))
                         elif true_parent.datatype != arg1_symbol.datatype:
-                            self.errors.append(SemanticError(node.pos_start, node.pos_end, f"Type mismatch 17: Expected '{true_parent.datatype}', got '{arg1_symbol.datatype}'"))
+                                self.errors.append(SemanticError(node.pos_start, node.pos_end, f"Type mismatch 17: Expected '{true_parent.datatype}', got '{arg1_symbol.datatype}'"))
                     else:
                         self.errors.append(SemanticError(node.pos_start, node.pos_end, f"Expected clan or string, got '{arg1_type}'"))
 
                 if isinstance(arg2, IdNode) and not self.symbol_table.get(arg2.name):
-                    self.errors.append(SemanticError(node.pos_start, node.pos_end, f"Undeclared variable 9 '{arg2.name}'"))
+                    self.errors.append(SemanticError(node.pos_start, node.pos_end, f"Undeclared variable 11 '{arg2.name}'"))
                     continue
                 else: 
                     if not arg2_type == 'int':
                         self.errors.append(SemanticError(node.pos_start, node.pos_end, f"Expected 'int', got '{arg2_type}'"))
 
                 if isinstance(arg3, IdNode) and not self.symbol_table.get(arg3.name):
-                    self.errors.append(SemanticError(node.pos_start, node.pos_end, f"Undeclared variable 10 '{arg3.name}'"))
+                    self.errors.append(SemanticError(node.pos_start, node.pos_end, f"Undeclared variable 12 '{arg3.name}'"))
+                    continue
                 else:
                     if not arg3_type == 'int':
                         self.errors.append(SemanticError(node.pos_start, node.pos_end, f"Expected 'int', got '{arg3_type}'"))
-                    
-            elif isinstance(node, DismantleNode):
-                arg1 = node.value
-                arg2 = node.delimiter
 
-                arg1_type = self.infer_type(arg1)
-                arg2_type = self.infer_type(arg2)
-
-                if isinstance(arg1, IdNode) and not self.symbol_table.get(arg1.name):
-                    self.errors.append(SemanticError(node.pos_start, node.pos_end, f"Undeclared variable 11 '{arg1.name}'"))
-                    continue
-                else:
-                    if arg1_type != 'string':
-                        self.errors.append(SemanticError(node.pos_start, node.pos_end, f"Expected 'string', got '{arg1_type}'"))
-
-                if isinstance(arg2, IdNode) and not self.symbol_table.get(arg2.name):
-                    self.errors.append(SemanticError(node.pos_start, node.pos_end, f"Undeclared variable 12 '{arg2.name}'"))
-                else:
-                    if arg2_type != 'string':
-                        self.errors.append(SemanticError(node.pos_start, node.pos_end, f"Expected 'string', got '{arg2_type}'"))
 
         print(f"Unresolved cases after resolution: {self.unresolved_cases}\n")
 
